3502d6886cc35b46c751b38527dbd257b8b1c011
LexaMV <lexamv@gmail.com>
2019-01-06 18:42:09 +0300

    Add Panel

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,11 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-  
+  public int intRecord;
   public TextMeshProUGUI record;
   public TextMeshProUGUI score;
+
+     void SaveRecordPoints() {
+
+         intRecord = Convert.ToInt32(record.text);
+         PlayerPrefs.SetInt("RecordScore", intRecord);
+         PlayerPrefs.Save();
+     }
+
+     void LoadRecordPoints() {
+          intRecord = PlayerPrefs.GetInt("RecordScore");
+          record.text = intRecord.ToString();
+     }
+
+      public void UpdateRecordPoint() {
+          if (Convert.ToInt32(record.text) < Convert.ToInt32(score.text)) {
+               record.text = score.text;
+          }
+     }
+
+
 }
