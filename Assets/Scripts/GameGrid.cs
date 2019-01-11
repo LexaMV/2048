59fb5e227704c83a8b09bcb3874bdf38b38da3ac
LexaMV <LexaMV@gmail.com>
2019-01-11 14:11:26 +0300

    Сorrection by remarks

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -15,28 +15,27 @@ public class GameGrid : MonoBehaviour {
 		public Color color;
 	}
 	public Map[] colors;
-	public GameObject cubik;
-	public GameObject gameRectangle;
+	public GameObject backGroundCubePrefab;
+	public GameObject gameCubePrefab;
 	private int height;
 	private int weight;
 	public static float POS_SCALE = 1f;
 	public static float POS_OFFSET_X = 0;
 	public static float POS_OFFSET_Y = 0;
-	public GameObject backGroundBlocks;
-	public GameObject gameBlocks;
-	private GameObject[, ] gameGridBackcgound;
-	private GameObject[, ] game;
+	public Transform UIBackGroundBlock;
+	public Transform UIGameBlock;
+	private GameObject[, ] containerGameGridBackgound;
+	private GameObject[, ] gameArray;
 
 	public void LevelStart (int height, int weight) {
 		this.height = height;
-		this.weight = height;
-
-		CreateGameArray (height, weight);
+		this.weight = weight;
+		CreateArrays (height, weight);
 		CreateBackgroundGameGrid (height, weight);
 		CreateGameBlock (height, weight, -5);
 	}
 
-	private Color AddColorToBlock (int a) {
+	private Color GetColorToBlock (int a) {
 		Color color = new Color ();
 		foreach (var m in colors) {
 			if (m.number == a) {
@@ -47,17 +46,17 @@ public class GameGrid : MonoBehaviour {
 		return color;
 	}
 
-	public void CreateGameArray (int height, int weight) {
-		gameGridBackcgound = new GameObject[height, weight];
-		game = new GameObject[height, weight];
+	public void CreateArrays (int height, int weight) {
+		containerGameGridBackgound = new GameObject[height, weight];
+		gameArray = new GameObject[height, weight];
 	}
 
 	public void CreateBackgroundGameGrid (int height, int weight) {
 
 		for (int i = 0; i < height; i++) {
 			for (int u = 0; u < weight; u++) {
-				gameGridBackcgound[i, u] = Instantiate (cubik, FixTransformBlockXY (i, u), Quaternion.identity);
-				gameGridBackcgound[i, u].transform.SetParent (backGroundBlocks.transform);
+				containerGameGridBackgound[i, u] = Instantiate (backGroundCubePrefab, FixTransformBlockXY (i, u), Quaternion.identity);
+				containerGameGridBackgound[i, u].transform.SetParent (UIbackGroundBlocks);
 			}
 		}
 	}
@@ -65,12 +64,12 @@ public class GameGrid : MonoBehaviour {
 
 		int a = UnityEngine.Random.Range (0, height);
 		int b = UnityEngine.Random.Range (0, weight);
-		if (game[a, b] == null) {
-			game[a, b] = Instantiate (gameRectangle, FixTransformBlockXYZ (a, b, z), Quaternion.identity);
-			game[a, b].transform.SetParent (gameBlocks.transform);
-			int s = Convert.ToInt32 (game[a, b].transform.Find ("Text").GetComponent<TextMeshPro> ().text);
-			game[a, b].GetComponent<SpriteRenderer> ().color = AddColorToBlock (s);
-			game[a, b].transform.DOScale (0.15f, 0.1f).SetLoops (2, LoopType.Yoyo);
+		if (gameArray[a, b] == null) {
+			gameArray[a, b] = Instantiate (gameCubePrefab, FixTransformBlockXYZ (a, b, z), Quaternion.identity);
+			gameArray[a, b].transform.SetParent (UIGameBlocks);
+			int s = Convert.ToInt32 (gameArray[a, b].transform.Find ("Text").GetComponent<TextMeshPro> ().text);
+			gameArray[a, b].GetComponent<SpriteRenderer> ().color = GetColorToBlock (s);
+			gameArray[a, b].transform.DOScale (0.15f, 0.1f).SetLoops (2, LoopType.Yoyo);
 		} else {
 			CreateGameBlock (height, weight, -5);
 		}
@@ -79,13 +78,13 @@ public class GameGrid : MonoBehaviour {
 	public void GoBlockDown (bool yes) {
 		for (int i = 0; i < height; i++) {
 			for (int u = 0; u < weight; u++) {
-				if (game[i, u] == null && u + 1 != weight) {
+				if (gameArray[i, u] == null && u + 1 != weight) {
 					for (int o = u; o < weight; o++) {
-						if (o + 1 != weight && game[i, o + 1] != null) {
-							GameObject gameObject = game[i, u];
-							game[i, o + 1].transform.DOMove (new Vector3 (i, u, -5), 0f);
-							game[i, u] = game[i, o + 1];
-							game[i, o + 1] = gameObject;
+						if (o + 1 != weight && gameArray[i, o + 1] != null) {
+							GameObject gameObject = gameArray[i, u];
+							gameArray[i, o + 1].transform.DOMove (new Vector3 (i, u, -5), 0f);
+							gameArray[i, u] = gameArray[i, o + 1];
+							gameArray[i, o + 1] = gameObject;
 							break;
 						}
 					}
@@ -103,13 +102,13 @@ public class GameGrid : MonoBehaviour {
 		for (int i = 0; i < height; i++) {
 			for (int u = weight - 1; u > -1; u--) {
 				Debug.Log ("i = " + i + " , " + " u = " + u);
-				if (game[i, u] == null && u - 1 != -1) {
+				if (gameArray[i, u] == null && u - 1 != -1) {
 					for (int o = u; o > -1; o--)
-						if (o - 1 != -1 && game[i, o - 1] != null) {
-							GameObject gameObject = game[i, u];
-							game[i, o - 1].transform.DOMove (new Vector3 (i, u, -5), 0f);
-							game[i, u] = game[i, o - 1];
-							game[i, o - 1] = gameObject;
+						if (o - 1 != -1 && gameArray[i, o - 1] != null) {
+							GameObject gameObject = gameArray[i, u];
+							gameArray[i, o - 1].transform.DOMove (new Vector3 (i, u, -5), 0f);
+							gameArray[i, u] = gameArray[i, o - 1];
+							gameArray[i, o - 1] = gameObject;
 							break;
 						}
 				}
@@ -125,13 +124,13 @@ public class GameGrid : MonoBehaviour {
 		for (int u = 0; u < height; u++) {
 			for (int i = 0; i < height; i++) {
 				// Debug.Log("i = " + i + " , " + " u = " + u);
-				if (game[i, u] == null && i + 1 != weight) {
+				if (gameArray[i, u] == null && i + 1 != weight) {
 					for (int o = i; o < weight; o++) {
-						if (o + 1 != weight && game[o + 1, u] != null) {
-							GameObject gameObject = game[i, u];
-							game[o + 1, u].transform.DOMove (new Vector3 (i, u, -5), 0f);
-							game[i, u] = game[o + 1, u];
-							game[o + 1, u] = gameObject;
+						if (o + 1 != weight && gameArray[o + 1, u] != null) {
+							GameObject gameObject = gameArray[i, u];
+							gameArray[o + 1, u].transform.DOMove (new Vector3 (i, u, -5), 0f);
+							gameArray[i, u] = gameArray[o + 1, u];
+							gameArray[o + 1, u] = gameObject;
 							break;
 						}
 					}
@@ -149,13 +148,13 @@ public class GameGrid : MonoBehaviour {
 		for (int u = weight - 1; u > -1; u--) {
 			for (int i = weight - 1; i > -1; i--) {
 				Debug.Log ("i = " + i + " , " + " u = " + u);
-				if (game[i, u] == null) {
+				if (gameArray[i, u] == null) {
 					for (int o = i; o > -1; o--)
-						if (o - 1 != -1 && game[o - 1, u] != null) {
-							GameObject gameObject = game[i, u];
-							game[o - 1, u].transform.DOMove (new Vector3 (i, u, -5), 0f);
-							game[i, u] = game[o - 1, u];
-							game[o - 1, u] = gameObject;
+						if (o - 1 != -1 && gameArray[o - 1, u] != null) {
+							GameObject gameObject = gameArray[i, u];
+							gameArray[o - 1, u].transform.DOMove (new Vector3 (i, u, -5), 0f);
+							gameArray[i, u] = gameArray[o - 1, u];
+							gameArray[o - 1, u] = gameObject;
 							break;
 						}
 				}
@@ -170,14 +169,14 @@ public class GameGrid : MonoBehaviour {
 	private void CheckUP () {
 		for (int i = 0; i < height; i++) {
 			for (int u = weight - 1; u > -1; u--) {
-				if (game[i, u] != null && u - 1 != -1 && game[i, u - 1] != null) {
-					if (Convert.ToInt32 (game[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (game[i, u - 1].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
-						MultiplicationBy2 (game[i, u]);
-						SetScore (game[i, u]);
-						SetColor (game[i, u]);
+				if (gameArray[i, u] != null && u - 1 != -1 && gameArray[i, u - 1] != null) {
+					if (Convert.ToInt32 (gameArray[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (gameArray[i, u - 1].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
+						MultiplicationBy2 (gameArray[i, u]);
+						SetScore (gameArray[i, u]);
+						SetColor (gameArray[i, u]);
 						ui.UpdateRecordPoint ();
-						Destroy (game[i, u - 1].gameObject);
-						game[i, u - 1] = null;
+						Destroy (gameArray[i, u - 1].gameObject);
+						gameArray[i, u - 1] = null;
 						GoBlockUP (false);
 					}
 				}
@@ -188,14 +187,14 @@ public class GameGrid : MonoBehaviour {
 	private void CheckRight () {
 		for (int u = weight - 1; u > -1; u--) {
 			for (int i = 0; i < height; i++) {
-				if (game[i, u] != null && i - 1 != -1 && game[i - 1, u] != null) {
-					if (Convert.ToInt32 (game[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (game[i - 1, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
-						MultiplicationBy2 (game[i, u]);
-						SetScore (game[i, u]);
-						SetColor (game[i, u]);
+				if (gameArray[i, u] != null && i - 1 != -1 && gameArray[i - 1, u] != null) {
+					if (Convert.ToInt32 (gameArray[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (gameArray[i - 1, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
+						MultiplicationBy2 (gameArray[i, u]);
+						SetScore (gameArray[i, u]);
+						SetColor (gameArray[i, u]);
 						ui.UpdateRecordPoint ();
-						Destroy (game[i - 1, u].gameObject);
-						game[i - 1, u] = null;
+						Destroy (gameArray[i - 1, u].gameObject);
+						gameArray[i - 1, u] = null;
 						GoBlockUP (false);
 					}
 				}
@@ -206,14 +205,14 @@ public class GameGrid : MonoBehaviour {
 	private void CheckLeft () {
 		for (int u = 0; u < height; u++) {
 			for (int i = 0; i < weight; i++) {
-				if (game[i, u] != null && i + 1 != weight && game[i + 1, u] != null) {
-					if (Convert.ToInt32 (game[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (game[i + 1, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
-						MultiplicationBy2 (game[i, u]);
-						SetScore (game[i, u]);
-						SetColor (game[i, u]);
+				if (gameArray[i, u] != null && i + 1 != weight && gameArray[i + 1, u] != null) {
+					if (Convert.ToInt32 (gameArray[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (gameArray[i + 1, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
+						MultiplicationBy2 (gameArray[i, u]);
+						SetScore (gameArray[i, u]);
+						SetColor (gameArray[i, u]);
 						ui.UpdateRecordPoint ();
-						Destroy (game[i + 1, u].gameObject);
-						game[i + 1, u] = null;
+						Destroy (gameArray[i + 1, u].gameObject);
+						gameArray[i + 1, u] = null;
 						GoBlockLeft (false);
 					}
 				}
@@ -224,14 +223,14 @@ public class GameGrid : MonoBehaviour {
 	private void CheckDown () {
 		for (int i = 0; i < height; i++) {
 			for (int u = 0; u < weight; u++) {
-				if (game[i, u] != null && u + 1 != weight && game[i, u + 1] != null) {
-					if (Convert.ToInt32 (game[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (game[i, u + 1].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
-						MultiplicationBy2 (game[i, u]);
-						SetScore (game[i, u]);
-						SetColor (game[i, u]);
+				if (gameArray[i, u] != null && u + 1 != weight && gameArray[i, u + 1] != null) {
+					if (Convert.ToInt32 (gameArray[i, u].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ()) == Convert.ToInt32 (gameArray[i, u + 1].transform.Find ("Text").GetComponent<TextMeshPro> ().text.ToString ())) {
+						MultiplicationBy2 (gameArray[i, u]);
+						SetScore (gameArray[i, u]);
+						SetColor (gameArray[i, u]);
 						ui.UpdateRecordPoint ();
-						Destroy (game[i, u + 1].gameObject);
-						game[i, u + 1] = null;
+						Destroy (gameArray[i, u + 1].gameObject);
+						gameArray[i, u + 1] = null;
 						GoBlockDown (false);
 					}
 				}
@@ -245,7 +244,7 @@ public class GameGrid : MonoBehaviour {
 
 	private void SetColor (GameObject gameObject) {
 		int s = Convert.ToInt32 (gameObject.transform.Find ("Text").GetComponent<TextMeshPro> ().text);
-		gameObject.GetComponent<SpriteRenderer> ().color = AddColorToBlock (s);
+		gameObject.GetComponent<SpriteRenderer> ().color = GetColorToBlock (s);
 	}
 
 	private void SetScore (GameObject gameObject) {
